cc5cdb900d04902d29ce792207ed7b0ced5b6275
sardariuss <sardariuss@gmail.com>
2024-06-26 11:00:21 -0400

    Add frontend

---
+++ src/frontend/App.tsx
@@ -0,0 +1,10 @@
+
+function App () {
+    return (
+        <div className="flex flex-col min-h-screen w-full bg-white dark:bg-slate-900 dark:border-gray-700 justify-between">
+            GRRRUNT APP
+        </div>
+    )
+}
+
+export default App;
